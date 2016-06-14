ae7a8cdd4ae5f1f9b65e521265028aee244a4f6a
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-14 09:14:12 +0000

    Added starting page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 News::Application.routes.draw do
+
+  root 'start_page#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
